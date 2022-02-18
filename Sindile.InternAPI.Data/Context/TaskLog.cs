af39a4eb7ae022b315d67db2b73da1200f6ded85
MceeX <sindileedmonds@gmail.com>
2022-02-18 19:40:12 +0200

    - Authorization token

--- Sindile.InternAPI.Data/Context/TaskLog.cs
+++ Sindile.InternAPI.Data/Context/TaskLog.cs
@@ -3,14 +3,14 @@
     public partial class TaskLog
     {
         public int Id { get; set; }
-        public int InternId { get; set; }
+        public int UserId { get; set; }
         public int TaskId { get; set; }
         public double Duration { get; set; }
         public DateTime DateTime { get; set; }
         public bool? Deleted { get; set; }
         public double HourlyRate { get; set; }
 
-        public virtual Occupation Intern { get; set; }
+        public virtual Occupation Occupation { get; set; }
         public virtual Task Task { get; set; }
     }
 }
